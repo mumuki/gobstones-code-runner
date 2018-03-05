b66c4c4f4146aaa906e4245d7bae8dedf60918eb
Rodrigo Alfonso <rodri042@gmail.com>
2018-03-05 19:49:32 -0300

    Using --strip-exclude

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 mkdir -p dist
-vulcanize --inline-scripts --inline-css --exclude "bower_components/polymer/polymer.html" gobstones-code-runner.html > dist/gobstones-code-runner.html
+vulcanize --inline-scripts --inline-css --strip-exclude "bower_components/polymer/polymer.html" gobstones-code-runner.html > dist/gobstones-code-runner.html
